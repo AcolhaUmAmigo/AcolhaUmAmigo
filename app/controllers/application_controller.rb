7b67774e801095f9d4d4d7522f99503240aed293
henriquebortoletti <henriquebortoletti@gmail.com>
2017-09-08 03:07:04 -0300

    arrumando as cagadas q deu

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+	def hello
+	render html: 'Acolha um amigo
+		<img src="http://tudosobrecachorros.com.br/wp-content/uploads/precos-de-cachorro.png"/>'.html_save
+
+end
+end
