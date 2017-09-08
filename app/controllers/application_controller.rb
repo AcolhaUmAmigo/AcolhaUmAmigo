42fb983590c0ec12cebc6b165971f74fba187be3
henriquebortoletti <henriquebortoletti@gmail.com>
2017-09-08 03:35:10 -0300

    home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 	def hello
-	render html: 'Acolha um amigo
+	render html: '<h1>Acolha um amigo</h1>
 		<img src="http://tudosobrecachorros.com.br/wp-content/uploads/precos-de-cachorro.png"/>'.html_safe
 
 end
